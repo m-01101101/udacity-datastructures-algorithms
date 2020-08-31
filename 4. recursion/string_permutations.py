52c7ccc09c2b428e710700ddfcbc8b2d58d4ccdb
michael <michaelrogers.arsenal@gmail.com>
2020-08-31 09:23:25 +0100

    Updates on big O

---
+++ 4. recursion/string_permutations.py
@@ -0,0 +1,19 @@
+"""
+Given an input string, return all permutations of the string in an array.
+
+Example 1:
+* `string = 'ab'`
+* `output = ['ab', 'ba']`
+
+Example 2:
+* `string = 'abc'`
+* `output = ['abc', 'bac', 'bca', 'acb', 'cab', 'cba']`
+
+Strings in Python are immutable, 
+    which means that we cannot overwrite the characters of the String objects.
+    we can only re-assign the variable to a new value (string)
+    therefore, we do not require a deep copy
+
+
+"""
+
