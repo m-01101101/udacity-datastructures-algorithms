31918d690a8b31b5ec9e5810038bc6a0546e08eb
michael <michaelrogers.arsenal@gmail.com>
2020-09-14 22:41:59 +0100

    Black formatting

--- 3. data_structures/hash_map/longest_sequence.py
+++ 3. data_structures/hash_map/longest_sequence.py
@@ -13,11 +13,12 @@ If two subsequences are of equal length,
 """
 from typing import List
 
+
 def longest_consecutive_subsequence(input_list: List[int]) -> List[int]:
     tracking_dict = dict()
     for index, value in enumerate(input_list):
         tracking_dict[value] = index
-    
+
     start_index = max_len = -1
 
     for index, value in enumerate(input_list):
@@ -38,67 +39,68 @@ def longest_consecutive_subsequence(input_list: List[int]) -> List[int]:
         while increment_bwd in tracking_dict and tracking_dict[increment_bwd] > 0:
             track_len += 1
             tracking_dict[increment_bwd] = -1
-            increment_bwd -= 1   
+            increment_bwd -= 1
 
-        #TODO implement if function         
+        # TODO implement if function
+
 
 def udacity_longest_consecutive_subsequence(input_list):
-    
+
     # Create a dictionary.
     # Each element of the input_list would become a "key", and
     # the corresponding index in the input_list would become the "value"
     element_dict = dict()
 
     # Traverse through the input_list, and populate the dictionary
-    # Time complexity = O(n) 
+    # Time complexity = O(n)
     for index, element in enumerate(input_list):
         element_dict[element] = index
 
     # Represents the length of longest subsequence
     max_length = -1
-    
-    # Represents the index of smallest element in the longest subsequence
-    starts_at = -1  
 
-    # Traverse again - Time complexity = O(n) 
+    # Represents the index of smallest element in the longest subsequence
+    starts_at = -1
+
+    # Traverse again - Time complexity = O(n)
     for index, element in enumerate(input_list):
 
         current_starts = index
-        element_dict[element] = -1         # Mark as visited
+        element_dict[element] = -1  # Mark as visited
 
-        current_count = 1                  # length of the current subsequence
+        current_count = 1  # length of the current subsequence
 
-        '''CHECK ONE ELEMENT FORWARD'''
-        current = element + 1              # `current` is the expected number
+        """CHECK ONE ELEMENT FORWARD"""
+        current = element + 1  # `current` is the expected number
 
         # check if the expected number is available (as a key) in the dictionary,
         # and it has not been visited yet (i.e., value > 0)
         # Time complexity: Constant time for checking a key and retrieving the value = O(1)
         while current in element_dict and element_dict[current] > 0:
-            current_count += 1             # increment the length of subsequence 
-            element_dict[current] = -1     # Mark as visited
-            current = current + 1          
+            current_count += 1  # increment the length of subsequence
+            element_dict[current] = -1  # Mark as visited
+            current = current + 1
 
-            
-        '''CHECK ONE ELEMENT BACKWARD'''
+        """CHECK ONE ELEMENT BACKWARD"""
         # Time complexity: Constant time for checking a key and retrieving the value = O(1)
-        current = element - 1             # `current` is the expected number
+        current = element - 1  # `current` is the expected number
 
-        while current in element_dict and element_dict[current] > 0:    
-            current_starts = element_dict[current]         # index of smallest element in the current subsequence
-            current_count += 1                             # increment the length of subsequence 
+        while current in element_dict and element_dict[current] > 0:
+            current_starts = element_dict[
+                current
+            ]  # index of smallest element in the current subsequence
+            current_count += 1  # increment the length of subsequence
             element_dict[current] = -1
             current = current - 1
 
-        '''If length of current subsequence >= max length of previously visited subsequence'''
+        """If length of current subsequence >= max length of previously visited subsequence"""
         if current_count >= max_length:
             if current_count == max_length and current_starts > starts_at:
                 continue
-            starts_at = current_starts            # index of smallest element in the current (longest so far) subsequence
-            max_length = current_count            # store the length of current (longest so far) subsequence
+            starts_at = current_starts  # index of smallest element in the current (longest so far) subsequence
+            max_length = current_count  # store the length of current (longest so far) subsequence
 
+    start_element = input_list[starts_at]  # smallest element in the longest subsequence
 
-    start_element = input_list[starts_at]          # smallest element in the longest subsequence
-
-    # return a NEW list starting from `start_element` to `(start_element + max_length)` 
+    # return a NEW list starting from `start_element` to `(start_element + max_length)`
     return [element for element in range(start_element, start_element + max_length)]
--- 3. data_structures/hash_map/pair_sum_target.py
+++ 3. data_structures/hash_map/pair_sum_target.py
@@ -10,6 +10,7 @@ Assume that the list does not have any duplicates.
 """
 from typing import List
 
+
 def pair_sum_to_target(input_list: List[int], target: int) -> List[int]:
     """Returns the index of two numbers in a last that when added equal the target"""
 
@@ -31,6 +32,7 @@ def test_function(test_case):
     else:
         False
 
+
 test_case_1 = [[1, 5, 9, 7], 8, [0, 3]]
 assert test_function(test_case_1) == True
 
@@ -47,9 +49,9 @@ def fast_pair_sum_to_target(input_list, target):
     # Each element of the input_list would become a "key", and
     # the corresponding index in the input_list would become the "value"
     index_dict = dict()
-    
+
     for index, element in enumerate(input_list):
-        
+
         # element + (element - target) = target
         # if this value in the list then that + element = target
         if (target - element) in index_dict:  # this is why we assume no duplicates
@@ -57,6 +59,7 @@ def fast_pair_sum_to_target(input_list, target):
 
         index_dict[element] = index
 
-    return [-1,-1] 
+    return [-1, -1]
+
 
 # could i use map(lambda, input_list)?
