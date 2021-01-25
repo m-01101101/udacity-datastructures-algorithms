9cab6f916e212b1cb972cc6f1743776bcebc786d
michael <michaelrogers.arsenal@gmail.com>
2021-01-25 17:59:27 +0000

    adding heaps and unbalanced trees

---
+++ 5. basic_algorithms/heap.py
@@ -0,0 +1,138 @@
+# TODO revisit heap implementation
+class Heap:
+    """Min heap implementation"""
+
+    def __init__(self):
+        self.storage = []  # initialise array
+        self.heap_size = 0  # len(self.storage) does not work
+
+    def __repr__(self) -> str:
+        return "".join(
+            [f"idx: {i[0]} " f"value: {i[1]}\n" for i in list(enumerate(self.storage))]
+        )
+
+    def _heapify_up(self):
+        child_idx = len(self.storage) - 1  # tried using -2 as we've already appended
+
+        # while c_idx > p_idx
+
+        while child_idx > 0:
+            # possible because we have a complete binary tree
+            parent_idx = (child_idx - 1) // 2
+            parent_element = self.storage[parent_idx]
+            child_element = self.storage[child_idx]
+
+            if parent_element > child_element:  # vice vera if implementing max heap
+                # swap
+                self.storage[parent_idx] = child_element
+                self.storage[child_idx] = parent_element
+
+                child_idx = parent_idx
+
+            else:
+                break
+
+    def _heapify_down(self):
+        parent_idx = 0
+
+        # -- TODO review this first while clause
+        while parent_idx < self.heap_size:  # this means i'll always do worst case O(n)
+            left_child_idx = 2 * parent_idx + 1
+            right_child_idx = 2 * parent_idx + 2
+
+            parent_element = self.storage[parent_idx]
+            left_child = right_child = None
+            min_element = parent_element
+
+            if left_child_idx < self.heap_size - 1:  # check left child exists
+                left_child = self.storage[left_child_idx]
+                min_element = min(parent_element, left_child)
+
+            if right_child_idx < self.heap_size - 1:  # check right child exists
+                right_child = self.storage[right_child_idx]
+                min_element = min(parent_element, right_child)
+
+            if min_element == parent_element:
+                return
+
+            if min_element == left_child:  # swap left and parent
+                self.storage[left_child_idx] = parent_element
+                self.storage[parent_idx] = min_element
+                parent_idx = left_child_idx
+
+            if min_element == right_child:  # swap right and parent
+                self.storage[right_child_idx] = parent_element
+                self.storage[parent_idx] = min_element
+                parent_idx = right_child_idx
+
+    def insert(self, data):
+        """
+        insertion must maintain heap order property (min or max)
+        (1) insert at the end O(1)
+        (2) heapify O(log(n)) -> bubble up
+        """
+        self.storage.append(data)
+        self.heap_size += 1
+        self._heapify_up()
+
+        # personally i prefer to append the new element and then heapify
+        #  it is perhaps more efficient to keep track of "next idx"
+        # in this case we have a self.next_idx attribute, init at 0
+        # and must add capacity when next_idx == len(storage)
+
+        # # add capacity
+        # self.storage[self.next_idx] = data
+        # self._max_heapify()
+        # self.next_idx += 1
+
+        # if self.next_idx > len(self.storage):
+        #     temp = [None] * (2 * len(self.storage))
+        #     self.storage.extend(temp)
+
+    def remove(self):
+        """
+        in a heap we only allow someone to remove the root node
+        hence, useful for priority queues
+        (1) remove the element O(1)
+        (2) make the last element added the root node and heapify -> bubble down
+        """
+        if self.heap_size == 0:
+            return None
+
+        root_element = self.storage[0]  #  to be returned
+
+        last_element = self.storage[-1]
+        self.storage[0] = last_element  # overwrite first element with last
+        self.storage.pop()  # remove the duplicate element
+        self.heap_size -= 1
+        self._heapify_down()
+
+        return root_element
+
+    def peek(self):
+        """Returns None if empty"""
+        return None if self.heap_size == 0 else self.storage[0]
+
+    def is_empty(self) -> bool:
+        return self.heap_size == 0
+
+
+myHeap = Heap()
+elements = [1, 2, 3, 4, 1, 2]
+
+for _ in elements:
+    myHeap.insert(_)
+
+# assert myHeap == "[(0, 1), (1, 1), (2, 2), (3, 2), (4, 3), (5, 4)]"
+# assert myHeap.heap_size == 6
+# assert myHeap.peek() == 1
+# assert myHeap.remove() == 1
+# assert myHeap.remove() == 1
+# assert myHeap.remove() == 2
+# assert myHeap.remove() == 2
+# assert myHeap.peek() == 3
+# assert myHeap.remove() == 3
+# assert myHeap.remove() == 4
+# assert myHeap.is_empty() == True
+# assert myHeap.peek() == None
+# assert myHeap.remove() == None
---
+++ 5. basic_algorithms/self_balancing_tree.py
@@ -0,0 +1,153 @@
+class Node(object):
+    def __init__(self, value, parent, color):
+        self.value = value
+        self.left = None
+        self.right = None
+        self.parent = parent
+        self.color = color
+
+    def __repr__(self):
+        print_color = "R" if self.color == "red" else "B"
+        return "%d%s" % (self.value, print_color)
+
+
+def grandparent(node):
+    if node.parent == None:
+        return None
+    return node.parent.parent
+
+
+# Helper for finding the node's parent's sibling
+def pibling(node):
+    p = node.parent
+    gp = grandparent(node)
+    if gp == None:
+        return None
+    if p == gp.left:
+        return gp.right
+    if p == gp.right:
+        return gp.left
+
+
+class RedBlackTree(object):
+    def __init__(self, root):
+        self.root = Node(root, None, "red")
+
+    def __iter__(self):
+        yield from self.root.__iter__()
+
+    def insert(self, new_val):
+        new_node = self.insert_helper(self.root, new_val)
+        self.rebalance(new_node)
+
+    def insert_helper(self, current, new_val):  # same logic used in binary tree
+        if current.value < new_val:
+            if current.right:
+                return self.insert_helper(current.right, new_val)
+            else:
+                current.right = Node(new_val, current, "red")
+                return current.right
+        else:
+            if current.left:
+                return self.insert_helper(current.left, new_val)
+            else:
+                current.left = Node(new_val, current, "red")
+                return current.left
+
+    def rebalance(self, node):
+        # case 1 root node
+        if node.parent == None:  # inserting the root node
+            return
+
+        # case 2 parent is black
+        if node.parent.color == "black":
+            # we inserted a red node beneath a black node
+            # the new children (the NULL nodes) are black by definition
+            # our red node replaced a black NULL node
+            # so the number of black nodes for any paths from parents is unchanged
+            # Nothing to do in this case
+            return
+
+        # case 3 red parent and red pibling
+        if pibling(node) and pibling(node).color == "red":  # pibling == parent sibling
+            # both parent and pibling are red,
+            # change them to black
+            # change grandparent - then recursion over grandparent
+            pibling(node).color = "black"
+            node.parent.color = "black"
+            grandparent(node).color = "red"
+            return self.rebalance(grandparent(node))
+
+        gp = grandparent(node)
+        # Small change, if there is no grandparent, cases 4 and 5
+        # won't apply
+        if gp == None:
+            return
+
+        # case 4, red parent, black pibling - inside case
+        if gp.left and node == gp.left.right:
+            self.rotate_left(node.parent)
+            node = node.left
+        elif gp.right and node == gp.right.left:
+            self.rotate_right(node.parent)
+            node = node.right
+
+        # case 5, red parent, black pibling - outside case
+        p = node.parent
+        gp = p.parent
+        if node == p.left:
+            self.rotate_right(gp)
+        else:
+            self.rotate_left(gp)
+        p.color = "black"
+        gp.color = "red"
+
+    def rotate_left(self, node):
+        # Save off the parent of the sub-tree we're rotating
+        p = node.parent
+
+        node_moving_up = node.right
+        # After 'node' moves up, the right child will now be a left child
+        node.right = node_moving_up.left
+
+        # 'node' moves down, to being a left child
+        node_moving_up.left = node
+        node.parent = node_moving_up
+
+        # Now we need to connect to the sub-tree's parent
+        # 'node' may have been the root
+        if p != None:
+            if node == p.left:
+                p.left = node_moving_up
+            else:
+                p.right = node_moving_up
+        node_moving_up.parent = p
+
+    def rotate_right(self, node):
+        p = node.parent
+
+        node_moving_up = node.left
+        node.left = node_moving_up.right
+
+        node_moving_up.right = node
+        node.parent = node_moving_up
+
+        # Now we need to connect to the sub-tree's parent
+        if p != None:
+            if node == p.left:
+                p.left = node_moving_up
+            else:
+                p.right = node_moving_up
+        node_moving_up.parent = p
+
+
+# TODO
+# Current implementation will add duplicates of the same value.
+# Change the implementation to mark how many times the same value has been inserted.
+
+# Implement search and see how it remains logarithmic for large data sets
+# def search(self, find_val):
+#     return False
+
+# Tinker with the rotations and early escapes to see how they break (use `print_tree`)
+# Consider adding deletion or sketching out how it should work
